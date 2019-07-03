8b0f97ffd3928bfb79bd5a4844b2ea06be727c6c
ZFY <44965494+EZFY@users.noreply.github.com>
2019-07-03 15:26:29 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+	 
+	<router-view></router-view>
+  </div>
+</template>
+
+<script>
+	import Login from './components/login'
+	// import MainHome from './pages/MainHome'
+	// import Top from './components/Top'
+	// import Asider from './components/Asider'
+export default {
+  name: 'App',
+	components:{
+		Login,
+		// MainHome
+		// Top,
+		// Asider
+		
+	}
+}
+</script>
+
+<style>
+	*{
+		margin: 0;
+		padding: 0;
+	}
+	.main{
+	position: fixed;
+	top:50px;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	display: flex;
+	background: #FFFFFF;
+}
+
+</style>
